2f3eb94a5c963e487937bb91276e8faaee2878ae
Dan Sayers <contact@dan-sayers.com>
2021-06-02 23:32:38 +1000

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Tabata',
-    version='0.1',
+    version='0.1.1',
     packages=['tabata',],
     license='MIT',
     long_description='Programmatic, Template Driven Tableau Workbooks',
