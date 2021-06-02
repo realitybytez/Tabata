7a76b57fd41565619a356f75857bf11d67845ff5
Dan Sayers <contact@dan-sayers.com>
2021-06-02 23:14:15 +1000

    Pypi restructure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Tabata',
+    version='0.1',
+    packages=['tabata',],
+    license='MIT',
+    long_description='Programmatic, Template Driven Tableau Workbooks',
+)
